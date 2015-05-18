d9b216b2d78ad88798060ce0da481e10ac1df749
James Pharaoh <james@pharaoh.uk>
2015-05-18 15:11:51 +0000

    migrate to new style

--- lib/wbs/devops/client.py
+++ lib/wbs/devops/client.py
@@ -12,7 +12,10 @@ class Client:
 		ca_cert = None,
 		client_cert = None,
 		client_key = None,
+		prefix = "",
 	):
+
+		self.prefix = prefix
 
 		if peers:
 
@@ -31,43 +34,50 @@ class Client:
 	def exists (self, key):
 
 		try:
-			self.etcd_client.get (key)
+
+			self.etcd_client.get (
+				key = self.prefix + key)
+
 			return True
 
 		except etcd.EtcdKeyNotFound:
+
 			return False
 
 	def get_raw (self, key):
 
-		value_etcd = self.etcd_client.get (key)
+		value_etcd = self.etcd_client.get (
+			key = self.prefix + key)
 
 		return value_etcd.value
 
 	def set_raw (self, key, value):
 
-		self.etcd_client.write (key, value)
+		self.etcd_client.write (
+			key = self.prefix + key,
+			value = value)
 
 	def rm (self, key):
 
 		self.etcd_client.delete (
-			key = key)
+			key = self.prefix + key)
 
 	def rmdir (self, key):
 
 		self.etcd_client.delete (
-			key = key,
+			key = self.prefix + key,
 			dir = True)
 
 	def mkdir_queue (self, key):
 
 		result = self.etcd_client.write (
-			key = key,
+			key = self.prefix + key,
 			value = None,
 			append = True,
 			dir = True)
 
 		return (
-			str (result.key),
+			str (result.key [len (self.prefix):]),
 			str (result.createdIndex),
 		)
 
