afb50673d69003c884c4a33aacca7108163a4581
James Pharaoh <james@pharaoh.uk>
2015-06-30 14:09:57 +0000

    extend options for certificate database export

--- python/gridlinker/certificate/certificate.py
+++ python/gridlinker/certificate/certificate.py
@@ -3,12 +3,21 @@ from __future__ import absolute_import
 import collections
 
 Certificate = collections.namedtuple ("Certificate", [
+
 	"serial",
 	"digest",
+
 	"certificate",
-	"private_key",
 	"certificate_path",
+
+	"chain",
+	"chain_paths",
+
+	"private_key",
 	"private_key_path",
+
+	"rsa_private_key",
+
 ])
 
 class AlreadyExistsError (Exception):
