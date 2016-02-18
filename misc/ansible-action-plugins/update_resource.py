8411f532c21b3c6095ce6c824a2a0eeae522f0bc
James Pharaoh <james.pharaoh@wistla.com>
2016-02-18 10:24:13 +0000

    fixes for new ansible

--- misc/ansible-action-plugins/update_resource.py
+++ misc/ansible-action-plugins/update_resource.py
@@ -4,10 +4,6 @@ from __future__ import unicode_literals
 import importlib
 import os
 
-#from ansible import utils
-#from ansible.runner.return_data import ReturnData
-#from ansible.utils import template
-
 from ansible.plugins.action import ActionBase
 
 class ActionModule (ActionBase):
